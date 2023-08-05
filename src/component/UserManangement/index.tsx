9ca9f6775c2ff770d26bbb999bb0b9b348024da8
Ridwan D <ridwan.d@nellika.co.th>
2023-08-05 14:06:31 +0700

    clear code

--- src/component/UserManangement/index.tsx
+++ src/component/UserManangement/index.tsx
@@ -8,24 +8,6 @@ import {
 const UserManagement = () => {
     const { data: userList } = useQuery("userList", () => { return getUsersApi() })
 
-
-    console.log("data", userList);
-
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
-        },
-    ];
-
     const columns = [
         {
             title: 'Name',
