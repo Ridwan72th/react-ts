c3cbc7169c759bbca49722441b13dd78ee18cec7
Ridwan D <ridwan.d@nellika.co.th>
2023-08-18 09:55:19 +0700

    fix : component loading

--- src/components/FormItem/Input/index.tsx
+++ src/components/FormItem/Input/index.tsx
@@ -2,15 +2,24 @@ import { Form, Input, } from 'antd'
 import { IFormInput } from './form-input.interface';
 
 const FormInput = (props: IFormInput) => {
-    const { name, label, message, rules, formPattern, required } = props;
+    const {
+        name,
+        label,
+        message,
+        rules,
+        formPattern,
+        required
+    } = props;
+
     const newMessage = message || `โปรดกรอกข้อมูล${label} !`
+
     return (
         <Form.Item
             name={name}
             label={label}
             rules={rules || [{ required: required, message: newMessage, pattern: formPattern }]}
         >
-            <Input {...props} required={false} />
+            <Input maxLength={50} {...props} required={false} />
         </Form.Item >
     )
 }
--- src/components/FormItem/Select/index.tsx
+++ src/components/FormItem/Select/index.tsx
@@ -2,14 +2,24 @@ import { Form, Select, } from 'antd'
 import { IFormSelect } from './form-select.interface';
 
 const FormSelect = (props: IFormSelect) => {
-    const { name, label, message, rules, formPattern, required } = props;
+    const {
+        name,
+        label,
+        message,
+        rules,
+        formPattern,
+        required,
+        loading = true
+    } = props;
+
     const newMessage = message || `โปรดเลือกข้อมูล${label} !`
     const { Option } = Select;
+
     return (
         <Form.Item name={name}
             label={label}
             rules={rules || [{ required: required, message: newMessage, pattern: formPattern }]}>
-            <Select {...props}>
+            <Select {...props} loading={loading}>
                 {props.options?.map((item, ind) => {
                     return <Option key={ind} value={item.value}>{item.label}</Option>
                 })}
