f75ea422e4a247597fb50ff1593065b7331b45ce
Ridwan D <ridwan.d@nellika.co.th>
2023-08-21 14:24:54 +0700

    fix : tailwind fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
   },
   theme: {
     extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+      "2xl": ['28px', '36px'],
+      "3xl": ['32px', '40px'],
+      "4xl": ['36px', '44px'],
+    }
   },
   plugins: [],
 }
