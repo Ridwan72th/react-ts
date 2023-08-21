f7a94ea7cf004cce865efebb91ef85bfefd7735b
Ridwan D <ridwan.d@nellika.co.th>
2023-08-21 13:11:24 +0700

    fix : layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,21 +1,12 @@
 import { Row, Col, } from 'antd'
 import Content from './Content'
-import SideMenu from './SideMenu'
 
 
 const Layout = () => {
     return (
         <div className='w-full'>
             <Row className='min-h-screen'>
-                <Col>
-                    <div className='h-[50px] flex justify-center items-center bg-orange-400 rounded-t-lg'>
-                        <div className='text-white'>Because We Care</div>
-                    </div>
-                    <SideMenu />
-                </Col>
                 <Col flex={1} >
-                    <div className='h-[50px]'>
-                    </div>
                     <Content />
                 </Col>
             </Row >
