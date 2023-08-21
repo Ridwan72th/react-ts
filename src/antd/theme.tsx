c93949c7b284d073d37a9ae249db9c39d7f224f0
Ridwan D <ridwan.d@nellika.co.th>
2023-08-21 11:00:46 +0700

    fix : primary button

--- src/antd/theme.tsx
+++ src/antd/theme.tsx
@@ -2,7 +2,7 @@ import { ThemeConfig } from "antd";
 
 export const theme: ThemeConfig | undefined = {
     token: {
-        colorPrimary: '#ff9800',
+        colorPrimary: '#b2d235',
         borderRadius: 8
     },
 }
