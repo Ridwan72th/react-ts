eed81f95898b4bb84336e10bb4fd07c98de2035d
Ridwan D <ridwan.d@nellika.co.th>
2023-08-23 15:19:41 +0700

    fix : sandbox modal method

--- src/containers/Sandbox.tsx
+++ src/containers/Sandbox.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Button, } from 'antd'
+import { ModalMethod } from 'src/components'
 
 const Sandbox = () => {
+
+    const handleModalSuccess = () => {
+        ModalMethod({
+            medthod: "success",
+            title: <div className='text-primary text-2xl text-center w-full pb-3'>รายการสำเร็จ</div>,
+            content: <div className='text-lg text-center w-full'>
+                {`อุ่นใจส่งเรื่องให้พี่เรียบร้อยแล้ว\nพนักงานจะตรวจสอบและแจ้งผลกลับให้พี่ทาง\nSMS ภายใน 3 ทำการนะฮะ`}
+            </div>
+        })
+    }
+
     return (
-        <div>Sandbox</div>
+        <div>
+            <Button onClick={handleModalSuccess}>Modal</Button>
+        </div>
     )
 }
 
