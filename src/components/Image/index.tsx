6795a62c21819decd9fc04541b951d044a31da49
Ridwan D <ridwan.d@nellika.co.th>
2023-08-23 11:28:41 +0700

    fix : image error on image component

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -6,10 +6,17 @@ const CImage = ({ src, width, height = "auto", alt, className, loading = true }:
 
     const antIcon = <LoadingOutlined style={{ fontSize: 30 }} />;
 
+    const handleErrorImage = ({ currentTarget }: any) => {
+        currentTarget.onerror = null;
+        currentTarget.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCDQ22xgk2PYGS_Y1OvfRUabKICuRBTP5HzgegXADIaf2qU_RIiOpTi5iwMge1-hAhOw4&usqp=CAU";
+    }
+
     return (
         <div>
             <Spin indicator={antIcon} style={{ maxHeight: "unset" }} spinning={loading} >
-                <img src={src} width={width} height={height} alt={alt} className={className} />
+                <img src={src} width={width} height={height} alt={alt} className={className}
+                    onError={handleErrorImage}
+                />
             </Spin>
         </div>
     )
