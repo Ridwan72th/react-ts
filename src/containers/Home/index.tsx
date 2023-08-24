0c0ec0fce4dfdb8c928de171e8e61331802828cb
Ridwan D <ridwan.d@nellika.co.th>
2023-08-24 13:58:27 +0700

    fix : form ของพรีเมี่ยมไม่ครบ

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -6,11 +6,11 @@ import {
     FormTextArea,
 } from "src/components/FormItem";
 import { transactionSubjects } from "src/constant";
+import { transformPlaceholder } from "src/helpers";
 
 const Home = () => {
     const [form] = Form.useForm();
     const subject = Form.useWatch('subject', form);
-
 
     const handleFinish = (values: any) => {
         console.log("finish ::", values);
@@ -35,63 +35,76 @@ const Home = () => {
                     </div>
 
                 </section>
-                <Form form={form} onFinish={handleFinish}
-                    layout="vertical" requiredMark={false}>
-                    <Row gutter={12}>
-                        <Col xs={24} lg={12}>
-                            <FormInputNumber
-                                name={"tel"} label={"ระบุเบอร์ที่แลกของพรีเมี่ยม"} required
-                                placeholder="เบอร์โทรศัพท์/เบอร์ไฟเบอร์" maxLength={10}
-                            />
-                        </Col>
-                        <Col xs={24} lg={12}>
-                            <FormInput name={"fullName"} label={"ชื่อผู้ติดต่อ"} required
-                                placeholder="ชื่อ-นามสกุล" />
-                        </Col>
-                        <Col xs={24} lg={24}>
-                            <FormSelect name={"subject"} label={"เรื่องที่ต้องการทำรายการ"} required
-                                placeholder="เลือกเรื่องที่ต้องการทำรายการ" loading={false} options={transactionSubjects} />
-                        </Col>
-                        <Col xs={24} lg={12}>
-                            <FormInputNumber
-                                name={"productNo"} label={"หมายเลขติดตามพัสดุ"} required
-                                placeholder="ระบุเลขติดตามพัสดุ" maxLength={10}
-                            />
-                        </Col>
-                        <Col xs={24} lg={12}>
-                            <FormInput
-                                name={"premiumToChange"} label={"ระบุของพรีเมี่ยมที่แลก"} required
-                                placeholder="ระบุรายละเอียดเพิ่มเติมของปัญหาที่พบ"
+                <section>
+                    <Form form={form} onFinish={handleFinish}
+                        layout="vertical" requiredMark={false}>
+                        <Row gutter={12}>
+                            <Col xs={24} lg={12}>
+                                <FormInputNumber
+                                    name={"tel"} label={"ระบุเบอร์ที่แลกของพรีเมี่ยม"} required
+                                    placeholder={transformPlaceholder(subject)} maxLength={10}
+                                />
+                            </Col>
+                            <Col xs={24} lg={12}>
+                                <FormInput name={"fullName"} label={"ชื่อผู้ติดต่อ"} required
+                                    placeholder="ชื่อ-นามสกุล" />
+                            </Col>
+                            <Col xs={24} lg={24}>
+                                <FormSelect name={"subject"} label={"เรื่องที่ต้องการทำรายการ"} required
+                                    placeholder="เลือกเรื่องที่ต้องการทำรายการ" loading={false} options={transactionSubjects} />
+                            </Col>
+                            <Col xs={24} lg={12}>
+                                <FormInputNumber
+                                    name={"productNo"} label={"หมายเลขติดตามพัสดุ"} required
+                                    placeholder="ระบุเลขติดตามพัสดุ" maxLength={10}
+                                />
+                            </Col>
+                            <>
+                                {handleShowForm([
+                                    "ของพรีเมี่ยมชำรุด",
+                                    "ของพรีเมี่ยมไม่ตรงตามที่แลก",
+                                    "ติดตามการจัดส่งของพรีเมี่ยม"
+                                ]) && <Col xs={24} lg={12}>
+                                        <FormInput
+                                            name={"premiumToChange"} label={subject === "ติดตามการจัดส่งของพรีเมี่ยม" ? "ของพรีเมี่ยมที่ติดตามการจัดส่ง" : "ของพรีเมี่ยมที่แลก"} required
+                                            placeholder="ระบุของพรีเมี่ยมที่แลก"
+                                        />
+                                    </Col>}
+                                {handleShowForm("ของพรีเมี่ยมชำรุด") &&
+                                    <Col xs={24} lg={24}>
+                                        <FormTextArea name={"problemMore"} label={"ปัญหาที่พบเพิ่มเติม"} required
+                                            placeholder="ระบุที่อยู่ในการจัดส่งของพรีเมี่ยม" rows={3} />
+                                    </Col>}
+                                {handleShowForm("ของพรีเมี่ยมไม่ตรงตามที่แลก") &&
+                                    <Col xs={24} lg={24}>
+                                        <FormInput
+                                            name={"premiumToRecieve"} label={"ของพรีเมี่ยมที่ได้รับ"} required
+                                            placeholder="ระบุของที่ได้รับไม่ตรงตามที่แลก"
+                                        />
+                                    </Col>}
+                                {handleShowForm("ของพรีเมี่ยมไม่ครบ") &&
+                                    <Col xs={24} lg={24}>
+                                        <FormTextArea name={"problemMore"} label={"ของพรีเมี่ยมที่แลกและได้ไม่ครบ"} required
+                                            placeholder="ระบุรายละเอียดของที่ได้ไม่ครบ" rows={3} />
+                                    </Col>}
+                            </>
+                            <Col xs={24} lg={24}>
+                                <FormTextArea name={"problemMore"} label={"ที่อยู่ในการจัดส่ง"} required
+                                    placeholder="ระบุที่อยู่ในการจัดส่งของพรีเมี่ยม" />
+                            </Col>
+                            <Col span={24}>
+                                <Row gutter={12} justify="center">
+                                    <Col>
+                                        <Button htmlType="submit" type="primary" className="px-8 h-[35px]">
+                                            ส่งข้อมูล
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Form>
+                </section>
 
-                            />
-                        </Col>
-                        {handleShowForm("ของพรีเมี่ยมชำรุด") && <Col xs={24} lg={24}>
-                            <FormTextArea name={"problemMore"} label={"ปัญหาที่พบเพิ่มเติม"} required
-                                placeholder="ระบุที่อยู่ในการจัดส่งของพรีเมี่ยม" rows={3} />
-                        </Col>}
-                        {handleShowForm("ของพรีเมี่ยมไม่ตรงตามที่แลก") && <Col xs={24} lg={24}>
-                            <FormInput
-                                name={"premiumToRecieve"} label={"ของพรีเมี่ยมที่ได้รับ"} required
-                                placeholder="ระบุของที่ได้รับไม่ตรงตามที่แลก"
-
-                            />
-                        </Col>}
-                        <Col xs={24} lg={24}>
-                            <FormTextArea name={"problemMore"} label={"ที่อยู่ในการจัดส่ง"} required
-                                placeholder="ระบุที่อยู่ในการจัดส่งของพรีเมี่ยม" />
-                        </Col>
-
-                        <Col span={24}>
-                            <Row gutter={12} justify="center">
-                                <Col>
-                                    <Button htmlType="submit" type="primary" className="px-8 h-[35px]">
-                                        ส่งข้อมูล
-                                    </Button>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Form>
             </div >
         </div >
     )
